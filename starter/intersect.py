bd21b292659dce5b4e2c4883dc0088e1c66efa9f
willyamwu <wu.willy.i.am@gmail.com>
2023-10-20 02:08:00 -0400

    new script to detect intersections

--- starter/intersect.py
+++ starter/intersect.py
@@ -1,6 +1,6 @@
 import FreeCAD as App
 import FreeCADGui as Gui
-
+import numpy as np
 
 obj_name = "Sketch"
 line_name = "Sketch001"
@@ -16,73 +16,83 @@ line = doc.getObject(line_name)
 plane = doc.getObject(plane_name)
 
 
-def rayTracing():
-    if obj.Shape.isInside(x.Placement.Base, 1.0, True):
-        FreeCADGui.getDocument("starter_test").getObject(
-            "Body").ShapeColor = (1.0, 0.0, 0.0)
-        print("intersecting")
-    else:
-        FreeCADGui.getDocument("starter_test").getObject(
-            "Body").ShapeColor = (0.0, 1.0, 0.0)
-        print("not intersecting")
-    # FreeCADGui.getDocument("starter_test").getObject("Body").ShapeColor = (204, 204, 204)
+# Check if two points are equal within a given tolerance
+# def are_points_equal(point1, point2, tolerance=1e-6):
+#     return all(abs(p1 - p2) < tolerance for p1, p2 in zip(point1, point2))
 
 
-def are_points_equal(point1, point2, tolerance=1e-6):
-    # Check if two points are equal within a given tolerance
-    return all(abs(p1 - p2) < tolerance for p1, p2 in zip(point1, point2))
+def find_intersection_point(line_vertices, plane_vertices):
+    # Convert line to parametric form
+    line_direction = line_vertices[0] - line_vertices[1]
+    line_parametric(t): return line_start + t * line_direction
 
+    # Find two vectors in the plane
+    vector1 = plane_vertices[2] - plane_vertices[1]
+    vector2 = plane_vertices[3] - plane_vertices[1]
 
-def find_intersection_point(line_start, line_end, plane_vertices):
-    # Calculate the direction vector of the line segment
-    line_direction = [end - start for start, end in zip(line_start, line_end)]
+    # Find the normal vector
+    normal_vector = np.cross(vector1, vector2)
 
-    # Calculate the normal vector of the plane
-    v1 = [plane_vertices[1][i] - plane_vertices[0][i] for i in range(3)]
-    v2 = [plane_vertices[2][i] - plane_vertices[0][i] for i in range(3)]
-    normal = [
-        v1[1] * v2[2] - v1[2] * v2[1],
-        v1[2] * v2[0] - v1[0] * v2[2],
-        v1[0] * v2[1] - v1[1] * v2[0]
-    ]
+    constant = normal_vector[]
 
-    # Calculate the denominator for the parameter t
-    denominator = sum(line_direction[i] * normal[i] for i in range(3))
+    # t = np.dot(plane_point - line_start, plane_normal) / np.dot(line_direction, plane_normal)
 
-    # Check if the line and plane are not parallel
-    if abs(denominator) > 1e-6:
-        # Calculate the parameter t for the intersection point
-        t = sum((plane_vertices[0][i] - line_start[i])
-                * normal[i] for i in range(3)) / denominator
+    # if 0 <= t <= 1:
+    #     intersection_point = line_parametric(t)
+    #     if np.all(intersection_point == plane_point):
+    #         return "The line lies on the plane."
+    #     else:
+    #         return "The line intersects the plane."
+    # else:
+    #     return "The line does not intersect the plane."
 
-        # Calculate the intersection point
-        intersection_point = [line_start[i] + t *
-                              line_direction[i] for i in range(3)]
+    # # Calculate the direction vector of the line segment
+    # line_direction = [end - start for start, end in zip(line_start, line_end)]
 
-        return intersection_point
+    # # Calculate the normal vector of the plane
+    # v1 = [plane_vertices[1][i] - plane_vertices[0][i] for i in range(3)]
+    # v2 = [plane_vertices[2][i] - plane_vertices[0][i] for i in range(3)]
+    # normal = [
+    #     v1[1] * v2[2] - v1[2] * v2[1],
+    #     v1[2] * v2[0] - v1[0] * v2[2],
+    #     v1[0] * v2[1] - v1[1] * v2[0]
+    # ]
 
-    return None
+    # # Calculate the denominator for the parameter t
+    # denominator = sum(line_direction[i] * normal[i] for i in range(3))
+
+    # # Check if the line and plane are not parallel
+    # if abs(denominator) > 1e-6:
+    #     # Calculate the parameter t for the intersection point
+    #     t = sum((plane_vertices[0][i] - line_start[i])
+    #             * normal[i] for i in range(3)) / denominator
+
+    #     # Calculate the intersection point
+    #     intersection_point = [line_start[i] + t *
+    #                           line_direction[i] for i in range(3)]
+
+    #     return intersection_point
+
+    # return None
 
 
 def get_line_vetices(line):
     line_vertices = [[0 for i in range(3)] for j in range(2)]
+    line_vertices = np.empty((2, 3))
     for i in range(2):
         line_vertices[i][0] = line.Shape.Vertexes[i].X
         line_vertices[i][1] = line.Shape.Vertexes[i].Y
         line_vertices[i][2] = line.Shape.Vertexes[i].Z
-        # print(line.Shape.Vertexes[i].X)
-        # print(line.Shape.Vertexes[i].Y)
     return line_vertices
 
 
 def get_plane_vertices(plane):
     plane_vertices = [[0 for i in range(3)] for j in range(4)]
+    plane_vertices = np.empty((4, 3))
     for i in range(4):
         plane_vertices[i][0] = plane.Shape.Vertexes[i].X
         plane_vertices[i][1] = plane.Shape.Vertexes[i].Y
         plane_vertices[i][2] = plane.Shape.Vertexes[i].Z
-        # print(plane.Shape.Vertexes[i].X)
-        # print(plane.Shape.Vertexes[i].Y)
     return plane_vertices
 
 
@@ -94,23 +104,22 @@ plane_vertices = get_plane_vertices(obj)
 print(plane_vertices)
 
 
-# Find the intersection point of the line segment with the plane
-intersection_point = find_intersection_point(
-    line_vertices[0], line_vertices[1], plane_vertices)
+# Define the vertices of the plane as [x, y, z]
+plane_vertices = [
+    [0, 0, 0],
+    [1, 0, 0],
+    [1, 1, 0],
+]
 
-if intersection_point is not None:
-    # Check if the intersection point is within the boundaries of the plane
-    min_x = min(vertex[0] for vertex in plane_vertices)
-    max_x = max(vertex[0] for vertex in plane_vertices)
-    min_y = min(vertex[1] for vertex in plane_vertices)
-    max_y = max(vertex[1] for vertex in plane_vertices)
+# Define the start and end points of the line segment
+line_start = [0.5, 0.5, -1]
+line_end = [0.5, 0.5, 1]
 
-    if (
-        min_x <= intersection_point[0] <= max_x and
-        min_y <= intersection_point[1] <= max_y
-    ):
-        print("The line segment intersects the space within the plane.")
-    else:
-        print("The line segment does not intersect the space within the plane.")
-else:
-    print("The line segment is parallel to the plane and does not intersect.")
+
+
+
+# # Check if the line and plane intersect
+# if find_intersection_point(line_vertices[0], line_vertices[1], plane_vertices):
+#     print("The line and the plane intersect.")
+# else:
+#     print("The line and the plane do not intersect.")
---
+++ starter/section.py
@@ -0,0 +1,149 @@
+from sympy import Point3D, Plane, Line3D
+import numpy as np
+# import FreeCAD as App
+# import FreeCADGui as Gui
+import sympy as sp
+
+t = sp.symbols('t')
+
+u, v, s = sp.symbols('u, v, s')
+
+a1x, a1y, a1z = -26.551074, 29.194627000000004, 0.0
+a12x, a12y, a12z = 45.091893, 29.194627000000004, 0.0
+a13x, a13y, a13z = 45.091893, -34.722119000000006, 0.0
+p0x, p0y, p0z = -112.944015, -52.98405, 0.0
+p01x, p01y, p01z = 47.203128, -43.33413, 0.
+
+eq1 = sp.Eq(a1x + u * a12x + v * a13x - p0x - s * p01x, 0)
+eq2 = sp.Eq(a1y + u * a12y + v * a13y - p0y - s * p01y, 0)
+eq3 = sp.Eq(a1z + u * a12z + v * a13z - p0y - s * p01y, 0)
+ans = sp.solve((eq1, eq2, eq3), (u, v, s))
+print(ans)
+
+# a1x, a1y, a1z = 1, 0, 0
+# a12x, a12y, a12z = -1, 1, 0
+# a13x, a13y, a13z = -1, 0, 1
+# p0x, p0y, p0z = 0, 0, 0
+# p01x, p01y, p01z = 2, 2, 2
+
+# eq1 = sp.Eq(a1x + u * a12x + v * a13x - p0x - t * p01x, 0)
+# eq2 = sp.Eq(a1y + u * a12y + v * a13y - p0y - t * p01y, 0)
+# eq3 = sp.Eq(a1z + u * a12z + v * a13z - p0y - t * p01y, 0)
+# ans = sp.solve((eq1, eq2, eq3), (u, v, t))
+# print(ans)
+
+
+# # # get the 3D model document
+# doc = App.ActiveDocument
+# # # get the visual representation model document
+# gui_doc = Gui.ActiveDocument
+
+
+# Check if two points are equal within a given tolerance
+# def are_points_equal(point1, point2, tolerance=1e-6):
+#     return all(abs(p1 - p2) < tolerance for p1, p2 in zip(point1, point2))​
+
+def find_intersection_point(line_vertices, plane_vertices):
+
+    # Get direction
+    line_direction = np.subtract(line_vertices[1], line_vertices[0])
+    # line_parametric(t): return line_start + t * line_direction
+
+    print("ld:", line_direction)
+
+    # Find two vectors in the plane
+    vector1 = np.subtract(plane_vertices[1], plane_vertices[0])
+    vector2 = np.subtract(plane_vertices[2], plane_vertices[0])
+
+    print("v1:", vector1)
+    print("v2:", vector2)
+
+    # Find the normal vector
+    normal_vector = np.cross(vector1, vector2)
+
+    # line_x = line_vertices[0][0] + line_direction[0] * t
+    # line_y = line_vertices[0][1] + line_direction[1] * t
+    # line_z = line_vertices[0][2] + line_direction[2] * t
+
+    # plane_eq = normal_vector[0] * u + normal_vector[0] * -plane_vertices[0][1] +
+
+    # x = plane_vertices[0][0] + u * vector1[0] + v * vector2[0]
+    # y = plane_vertices[0][1] + u * vector1[1] + v * vector2[1]
+    # z = plane_vertices[0][2] + u * vector1[2] + v * vector2[2]
+
+    coeff1 = normal_vector[0]
+    coeff2 = normal_vector[1]
+    coeff3 = normal_vector[2]
+
+    sum = coeff1 * plane_vertices[0][0] + coeff2 * \
+        plane_vertices[0][1] + coeff3 * plane_vertices[0][2]
+
+    print(normal_vector)
+    print("sum: ", sum)
+
+    eq1 = sp.Eq(coeff1 * (line_vertices[0][0] + line_direction[0] * t) + coeff2 * (
+        line_vertices[0][1] + line_direction[1] * t) + coeff3 * (line_vertices[0][2] + line_direction[2] * t), sum)
+    ans = sp.solve(eq1, t)
+    print("answer", ans)
+
+    # print("normal vector: ", normal_vector)
+    # constant = 0
+    # for i in range(3):
+    #     constant += normal_vector[i] * plane_vertices[0][i]
+    #     print(plane_vertices[0][i])
+
+    # print(constant)
+
+
+def get_line_vertices(line):
+    line_vertices = [[0 for i in range(3)] for j in range(2)]
+    for i in range(2):
+        line_vertices[i][0] = line.Shape.Vertexes[i].X
+        line_vertices[i][1] = line.Shape.Vertexes[i].Y
+        line_vertices[i][2] = line.Shape.Vertexes[i].Z
+    return line_vertices
+
+
+def get_plane_vertices(plane):
+    plane_vertices = [[0 for i in range(3)] for j in range(4)]
+    for i in range(4):
+        plane_vertices[i][0] = plane.Shape.Vertexes[i].X
+        plane_vertices[i][1] = plane.Shape.Vertexes[i].Y
+        plane_vertices[i][2] = plane.Shape.Vertexes[i].Z
+    return plane_vertices
+
+
+# obj_name = "Body"
+# obj = doc.getObject(obj_name)
+# rect_vert = get_plane_vertices(obj)
+# print("Rectangle vertexes:")
+# print(rect_vert)
+
+
+# line_name = "Sketch"
+# line = doc.getObject(line_name)
+# line_vert = get_line_vertices(line)
+# print("Line vertexes:")
+# print(line_vert)
+
+# find_intersection_point(line_vert, rect_vert)
+
+
+# if find_intersection_point(line_vert, rect_vert):
+#     print("The line and the plane intersect.")
+# else:
+#     print("The line and the plane do not intersect.")
+
+plane_vertices = np.array([
+    [-26.551074, 29.194627000000004, 0.0],
+    [45.091893, 29.194627000000004, 0.0],
+    [45.091893, -34.722119000000006, 0.0],
+    [-26.551074, -34.722119000000006, 0.0]
+])
+
+line_vertices = np.array([
+    [-112.944015, -52.98405, -2],
+    [64.359177, 26.171736, 2.]
+])
+
+find_intersection_point(line_vertices, plane_vertices)
